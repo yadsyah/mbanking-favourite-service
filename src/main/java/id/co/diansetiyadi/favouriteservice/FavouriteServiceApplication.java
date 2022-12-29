ad27f408c6fc833c46709d0612c218bf3233f3f7
yadsyah <diyansetiyadi@gmail.com>
2022-12-29 17:51:19 +0700

    init commit

---
+++ src/main/java/id/co/diansetiyadi/favouriteservice/FavouriteServiceApplication.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.favouriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavouriteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/diansetiyadi/favouriteservice/FavouriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.favouriteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouriteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
